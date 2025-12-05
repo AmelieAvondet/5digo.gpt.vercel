8d291a675eb4827dc566f8e364e5b5081b702ba0
ElChurry04 <santibensoria@gmail.com>
2025-12-05 12:29:43 -0300

    cambio de emoji

--- src/app/admin/courses/page.tsx
+++ src/app/admin/courses/page.tsx
@@ -86,7 +86,7 @@ export default function CoursesPage() {
                       onClick={handleLogout}
                       className="w-full text-left px-4 py-2 text-sm text-red-600 hover:bg-red-50 transition"
                     >
-                       Salir
+                      Salir
                     </button>
                   </div>
                 )}
